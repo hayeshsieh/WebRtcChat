438f4b5804c0740c4a61ba3e4732820fd785736d
hayeshsieh <203418040@gms.tku.edu.tw>
2017-06-14 22:44:49 +0800

    Initial Commit

---
+++ WebRtcChat/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WebRtcChat
+//
+//  Created by 謝旭棠 on 2017/6/14.
+//  Copyright © 2017年 謝旭棠. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
